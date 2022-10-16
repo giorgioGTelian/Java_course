fc0a5385904a6d2457bde4b793c585037fb3e5ef
giorgio <giorgiobiologist@gmail.com>
2022-10-16 23:03:41 +0200

    Create Summation.java

---
+++ Summation.java
@@ -0,0 +1,31 @@
+package en.codegym.task.pro.task04.task0403;
+
+import java.util.Scanner;
+
+/* 
+Summation
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        boolean isExit = false;
+        while (!isExit) {
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                sum = sum + number;
+            } else if (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.equals("ENTER")) {
+                    isExit = true;
+                }
+            }
+        }
+        System.out.println(sum);
+    }                           
+}
+
+    
+    
