df67097d141fc754964a43cdd26eaee04eabfb54
giorgio <giorgiobiologist@gmail.com>
2022-10-24 09:27:10 +0200

    Create Binary_converter.java

---
+++ Binary_converter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+//your code goes here
+public class Converter {
+    public static String toBinary(int num) {
+        String binary ="";
+        while(num > 0) {
+            binary = (num%2)+binary;
+            num/=2;
+            
+        }
+        return binary;
+    }
+}
+
+
+public class Program {
+    public static void main(String[ ] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.print(Converter.toBinary(x));
+    }
+} 
