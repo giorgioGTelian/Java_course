59a00ac53c1ef7088ae8af01d8c78856e9668d20
giorgio <giorgiobiologist@gmail.com>
2022-10-26 09:26:21 +0200

    Create BarkingDog.java

---
+++ UdemyCourse/BarkingDog.java
@@ -0,0 +1,15 @@
+public class BarkingDog {
+
+public static void main(String[ ] args) {
+
+
+
+ int HoursBark = (int) (Math.random() * (24 - 1)) + 1;
+ boolean DogBark = true;
+  
+  if () 
+ 
+ 
+}
+
+}
