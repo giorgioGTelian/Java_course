8acb75d024295ccc8db9ec7e079dd299da8845ef
giorgio <giorgiobiologist@gmail.com>
2022-12-06 11:41:08 +0100

    Update Obj_exercise_inheritance.java

--- CodeGym/Obj_exercise_inheritance.java
+++ CodeGym/Obj_exercise_inheritance.java
@@ -49,3 +49,34 @@ class Solution
        data[i] = new Point();
      printPoints(data);
    }
+
+   // Now let's add a method to the Point class that will display information about the object:
+   
+   public class Point
+{
+   public int x;
+   public int y;
+   public int color;
+   public void print()
+   {
+     System.out.println("Color of (" + x + ", " + y + ") = " + color);
+   }
+      
+      // Now the Solution class looks like this:
+
+class Solution
+{
+   public static void printPoints(Point[] points)
+   {
+     for (int i = 0; i < points.length; i++)
+       points[i].print();
+   }
+
+   public static void main(String[] args)
+   {
+     Point[] data = new Point[100];
+     for (int i = 0; i < data.length; i++)
+       data[i] = new Point();
+     printPoints(data);
+   }
+}
